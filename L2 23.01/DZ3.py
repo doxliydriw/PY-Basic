ea90e827229588e74637b3807d95adcdc6994c9a
doxliydriw <demcleany@gmail.com>
2023-01-26 22:49:25 +0100

    v.1.0

--- L2 23.01/DZ3.py
+++ L2 23.01/DZ3.py
@@ -1,23 +1,14 @@
 x = input('Input 5 digit number:')
 x = int(x)
 left, right = divmod(x, 10)
-a = right
-print(a)
-left, right = divmod(x, 100)
-b = right
-print (b)
-left, right = divmod(b, 10)
-b = left
-print(b)
-print (b,a)
-left, right = divmod(x, 1000)
-c = right
-print(c)
-left, right = divmod(x, 10000)
-d = right
-print(d)
-z = a + b + c + d
-print(type(z))
-int(z)
-print(type(z))
-print (z)
+a = right * 10000
+left, right = divmod(left, 10)
+a = a + (right * 1000)
+left, right = divmod(left, 10)
+a = a + (right * 100)
+left, right = divmod(left, 10)
+a = a + (right * 10)
+left, right = divmod(left, 10)
+a = a + right
+print (a)
+
