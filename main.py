66af943d10ea8dd20acc8e0873f2e963b01e19c6
doxliydriw <demcleany@gmail.com>
2023-02-06 17:04:37 +0100

    v.1.0

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import random
-lst = [1, 3, 5]
-lst1 = []
-lst1 = lst1.append(random.choice(lst)))
-print(lst1)
+lst = [0, 1, 0, 1, 0, 0, 0, 1, 0, 2, 3]
+lst0 = []
+x = 0
+y = 0
+while x <= lst.count(0) + 1:
+    lst0.append(lst.index(0, x))
+    x = lst.index(0, x) + 1
+x = lst.count(0)
+print(lst0)
+lst.sort()
+del(lst[0:(x)])
+print(lst)
+
+# while z <= lst(len):
+#     y = lst.index(z)
+#     print(lst.index(0))
+#
+# print(y)
+# lst0.append(y)
+# print(lst0)
