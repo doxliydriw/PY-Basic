d21ceb157d0a846fe3a60502d9f74fe4cb8d6e4e
doxliydriw <demcleany@gmail.com>
2023-02-07 22:22:57 +0100

    v.1.0

---
+++ L5 02.02/DZ11.py
@@ -0,0 +1,18 @@
+a = ['yes', 'YES', 'Yes', 'Y', 'y']
+b = ('y')
+while b in a:
+    x = float(input('Type 1st number:'))
+    z = input('Indicate action (+,-,*,/):')
+    y = float(input('Type 2nd number:'))
+    if z == '+':
+        print(x, z, y, '=', (x+y))
+    elif z == '-':
+        print(x, z, y, '=', (x-y))
+    elif z == '*':
+        print(x, z, y, '=', (x*y))
+    elif z == '/' and y != 0:
+        print(x, z, y, '=', (x/y))
+    else:
+        print('invalid data')
+    b = input('Another iteration? Y/N')
+print('The end')
--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-import keyword
-import string
-x = input('Input User name:')
-y = string.punctuation
-y = list(y) + [' ']
-z = 0
-del(y[26])
-for i in x:
-    if i.isupper():
-        print('False')
-        break
-    elif i in y:
-        print('False')
-        break
-    elif keyword.iskeyword(x):
-        print('False')
-        break
-    elif (x[0]).isdigit():
-        print('False')
-        break
-    else:
-        z = z + 1
-        if z == len(x):
-            print('True')
+a = ['yes', 'YES', 'Yes', 'Y', 'y']
+b = ('Yes')
+print(type(b))
+print(b in a)
