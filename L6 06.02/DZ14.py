7f828bc98f0d5b14775e496953620ef7eafe2d89
doxliydriw <demcleany@gmail.com>
2023-02-11 21:27:41 +0100

    v.1.0

--- L6 06.02/DZ14.py
+++ L6 06.02/DZ14.py
@@ -4,9 +4,9 @@ if int(x) > 0:
     while True:
         for i in x:
             res *= int(i)
-        if res >= 9:
-            x = str(res)
-            res = 1
-        else:
+        if res <= 9:
             print(res)
             break
+        else:
+            x = str(res)
+            res = 1
