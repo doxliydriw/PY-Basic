37199f9741b30b1105162e4a1ad1df89056801a4
doxliydriw <demcleany@gmail.com>
2023-02-16 11:38:20 +0100

    v.1.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add(a, b):
-    c = a + b
-    return c
-
-d = add(10, 10)
-print(d)
+text = "greetings, friends"
+text = text.replace(text[0], (text[0]).title(), 1)
+print(text)
+if text[-1] != '.':
+    text += '.'
+print(text)
