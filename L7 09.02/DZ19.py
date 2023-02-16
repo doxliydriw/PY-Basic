2849e3be21c0cccad1b666c5370c071b3123bbd8
doxliydriw <demcleany@gmail.com>
2023-02-16 17:08:50 +0100

    v.1.0

---
+++ L7 09.02/DZ19.py
@@ -0,0 +1,25 @@
+def common_elements():
+    import random
+    lst3 = []
+    lst5 = []
+    count = random.randint(10, 100)
+    i = 0
+    # print(count)
+    while i < count:
+        i += 1
+        lst_item = random.randint(0, 100)
+        # print(lst_item % 3, lst_item % 5)
+        if lst_item % 3 == 0 and lst_item % 5 == 0:
+            lst5.append(lst_item)
+            lst3.append(lst_item)
+        elif lst_item % 5 == 0:
+            lst5.append(lst_item)
+        elif lst_item % 3 == 0:
+            lst3.append(lst_item)
+    # print(lst3)
+    # print(lst5)
+    common = (set(lst3)).intersection(set(lst5))
+    return common
+
+
+print(common_elements())
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-text = 'Hello, hello'
-string = 'lo'
-y = text.count(string)
-if y > 1:
-    y = text.find(string) + 1
-    y = text.find(string, y)
-    print(type(y), y)
+import random
+lst3 = []
+lst5 = []
+count = random.randint(10, 100)
+i = 0
+print(count)
+while i < count:
+    i += 1
+    lst_item = random.randint(0, 100)
+    print(lst_item % 3, lst_item % 5)
+    if lst_item % 3 == 0 and lst_item % 5 == 0:
+        lst5.append(lst_item)
+        lst3.append(lst_item)
+    elif lst_item % 5 == 0:
+        lst5.append(lst_item)
+    elif lst_item % 3 == 0:
+        lst3.append(lst_item)
+print(lst3)
+print(lst5)
+common = (set(lst3)).intersection(set(lst5))
+print(common)
