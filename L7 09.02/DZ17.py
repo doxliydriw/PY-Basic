52e840102cd4ecd526d19bda6bea67dbc0d76247
doxliydriw <demcleany@gmail.com>
2023-02-16 11:41:07 +0100

    v.1.0

--- L7 09.02/DZ17.py
+++ L7 09.02/DZ17.py
@@ -1,11 +1,3 @@
-# def correct_sentence(text):
-#     pass
-# assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
-# assert correct_sentence("hello") == "Hello.", 'Test2'
-# assert correct_sentence("Greetings, friends") == "Greetings, friends.", 'Test3'
-# assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
-# assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
-# print('ОК')
 def correct_sentence(text):
     text = text.replace(text[0], (text[0]).title(), 1)
     if text[-1] != '.':
@@ -14,4 +6,8 @@ def correct_sentence(text):
 
 
 assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+# assert correct_sentence("hello") == "Hello.", 'Test2'
+# assert correct_sentence("Greetings, friends") == "Greetings, friends.", 'Test3'
+# assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+# assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
 print('ОК')
---
+++ L7 09.02/DZ18.py
@@ -0,0 +1,12 @@
+
+
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") == None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+
+def second_index(text, some_str):
+    pass
+
+
+print('ОК')
