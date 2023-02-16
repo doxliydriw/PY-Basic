1a1b2f6ae0fb1c4bcd811239dacd47326f4edd00
doxliydriw <demcleany@gmail.com>
2023-02-16 17:34:10 +0100

    v.1.0

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-import random
-lst3 = []
-lst5 = []
-count = random.randint(10, 100)
-i = 0
-print(count)
-while i < count:
-    i += 1
-    lst_item = random.randint(0, 100)
-    print(lst_item % 3, lst_item % 5)
-    if lst_item % 3 == 0 and lst_item % 5 == 0:
-        lst5.append(lst_item)
-        lst3.append(lst_item)
-    elif lst_item % 5 == 0:
-        lst5.append(lst_item)
-    elif lst_item % 3 == 0:
-        lst3.append(lst_item)
-print(lst3)
-print(lst5)
-common = (set(lst3)).intersection(set(lst5))
-print(common)
+some_list = [1, 2, 3, 4]
+some_str = "".join([str(y) for y in some_list])
+some_list.clear()
+some_str = str(int(some_str) + 1)
+for i in some_str:
+    some_list.append(int(i))
+print(some_list)
