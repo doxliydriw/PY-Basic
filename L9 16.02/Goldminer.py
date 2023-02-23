8f0bf44abd8f283a96033f2e0e955914a016d7f0
doxliydriw <123301390+doxliydriw@users.noreply.github.com>
2023-02-23 10:33:41 +0100

    v.1.0

---
+++ L9 16.02/Goldminer.py
@@ -0,0 +1,13 @@
+lst_mine = [7, [5, 8], [9, 8, 2], [1, 3, 5, 6], [6, 2, 4, 4, 5], [9, 5, 3, 5, 5, 7], [7, 4, 6, 4, 7, 6, 8]]
+lst_loop = []
+lst_total = []
+lst_summ = []
+
+
+def list_summ(lst_loop):
+    """Calculates summ of element on found path
+    :param lst_loop: list of 7 elemtns.
+    :return: a - semm of 7 elements in list.
+    """
+    a = sum(lst_loop)
+    return a
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-dict1 = {}
-some_list = [5, 5, 5, 2, 2, 0.5]
-for i in some_list:
-    dict1[i] = dict1.get(i, 0) + 1
-print(dict1)
-dict2 = dict1.copy()
-for i in dict1:
-    a = dict2.popitem()
-    if a[-1] == 1:
-        z = a[0]
-print(z)
-print(type(z))
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num = 8
+print("The factorial of", num, "is", factorial(num))
