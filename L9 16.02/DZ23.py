efb53a0192881021c1cf09ecb0eecdc01bf75cf2
doxliydriw <123301390+doxliydriw@users.noreply.github.com>
2023-02-25 15:22:25 +0100

    v.1.0

--- L9 16.02/DZ23.py
+++ L9 16.02/DZ23.py
@@ -1,6 +1,11 @@
-def popular_words (text, lst) ->
+def popular_words (text, findlst) -> dict:
     '''Function which gets the list of strings, calculates how many times each string is found in given sentance and
-    generates the dictionary where valueas are the srtings from the list and keys are number strings found in sentance'''
+    generates the dictionary where keys are the srtings from the list and values are number strings found in sentance'''
+    dict_one = {}
+    for i in findlst:
+        dict_one[i] = text.lower().split().count(i)
+        print(dict_one)
+    return(dict_one)
 
 
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 text = 'When I was One I had just begun When I was Two I was nearly new'
-print(text.count('was'))
-# for i in text.count()
-#     text.find('was')
+count = {}
+findlst = ['i', 'was', 'three', 'near']
+textlst = text.lower().split()
+print(textlst)
+v = 0
+for i in findlst:
+    for i in textlst:
+        textlst[v] == i:
+        v += 1
+        count[i] = count.get(i, 0) + 1
+        print(count)
+    v +=1
+
