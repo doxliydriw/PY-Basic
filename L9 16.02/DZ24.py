eaf1383f81975f2ea9a80846bea18bcf776e515b
doxliydriw <123301390+doxliydriw@users.noreply.github.com>
2023-02-26 16:25:00 +0100

    v.1.0

---
+++ L9 16.02/DZ24.py
@@ -0,0 +1,22 @@
+def difference(*args) -> int:
+    """Function calculates difference between minimum amd maximum number in set of numbers"""
+    nmbr_set = list(args)
+    if len(nmbr_set) == 0:
+        return 0
+    else:
+        maxnmbr = nmbr_set[0]
+        minnmbr = nmbr_set[0]
+        for i in nmbr_set:
+            if i > maxnmbr:
+                maxnmbr = i
+            elif i < minnmbr:
+                minnmbr = i
+    return round((maxnmbr - minnmbr), 2)
+
+
+assert difference(1, 2, 3) == 2, 'Test1'
+# assert difference (5, -5) == 10, 'Test2'
+# assert difference (10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+# assert difference () == 0, 'Test4'
+print('OK')
+
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-text = 'When I was One I had just begun When I was Two I was nearly new'
-count = {}
-findlst = ['i', 'was', 'three', 'near']
-textlst = text.lower().split()
-print(textlst)
-v = 0
-for i in findlst:
-    for i in textlst:
-        textlst[v] == i:
-        v += 1
-        count[i] = count.get(i, 0) + 1
-        print(count)
-    v +=1
-
+nmbr_set =  (-2, -1.2, -6, -9, -45)
+nmbr_set = list(nmbr_set)
+maxnmbr = nmbr_set[0]
+minnmbr = nmbr_set[0]
+for i in nmbr_set:
+    print(i)
+    if i > maxnmbr:
+        print('max', i > maxnmbr)
+        maxnmbr = i
+    elif i < minnmbr:
+        print('min', i < maxnmbr)
+        minnmbr = i
+print(maxnmbr,  minnmbr)
+print(maxnmbr - (minnmbr))
