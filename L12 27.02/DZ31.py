d98692ef0ca89d4fe5542b43abcdd2a8036e700a
doxliydriw <123301390+doxliydriw@users.noreply.github.com>
2023-03-07 00:33:39 +0100

    v.1.2

--- L12 27.02/DZ31.py
+++ L12 27.02/DZ31.py
@@ -1,30 +1,39 @@
-html_file = open("draft.html", "r", encoding='utf-8')
-line = ''
-cleanlist = []
-for text in html_file:
-    line += text
-    start_tag_count = line.count('<')
-    end_tag_count = line.count('>')
-    if start_tag_count == end_tag_count:
-        ix = 1
-        while ix <= start_tag_count:
-            a = line[(line.find('<')):(line.find('>')) + 1]
-            line = line.replace(a, '')
-            ix += 1
-        cleanlist.append(line)
+import codecs
+
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        # html_file = open("draft.html", "r", encoding='utf-8')
         line = ''
-clean_cell = 0
-cleanlist1 = []
-while clean_cell < len(cleanlist):
-    qw = cleanlist[clean_cell]
-    qw = qw.replace(' ', '').replace('\n', '')
-    if len(qw) != 0:
-        cleanlist1.append(cleanlist[clean_cell])
-    clean_cell += 1
-qw = cleanlist1.pop()
-qw = qw.replace('\n', '')
-cleanlist1.append(qw)
-clean1 = open("clean1.txt", "a", encoding='utf-8')
-clean1.writelines(cleanlist1)
-html_file.close()
-clean1.close()
+        cleanlist = []
+        for text in file:
+            line += text
+            start_tag_count = line.count('<')
+            end_tag_count = line.count('>')
+            if start_tag_count == end_tag_count:
+                ix = 1
+                while ix <= start_tag_count:
+                    a = line[(line.find('<')):(line.find('>')) + 1]
+                    line = line.replace(a, '')
+                    ix += 1
+                cleanlist.append(line)
+                line = ''
+        clean_cell = 0
+        cleanlist1 = []
+        while clean_cell < len(cleanlist):
+            qw = cleanlist[clean_cell]
+            qw = qw.replace(' ', '').replace('\n', '')
+            if len(qw) != 0:
+                cleanlist1.append(cleanlist[clean_cell])
+            clean_cell += 1
+        qw = cleanlist1.pop()
+        qw = qw.replace('\n', '')
+        cleanlist1.append(qw)
+        clean1 = open(result_file, "a", encoding='utf-8')
+        clean1.writelines(cleanlist1)
+        # html_file.close()
+        clean1.close()
+        open(result_file)
+
+
+delete_html_tags('draft.html')
